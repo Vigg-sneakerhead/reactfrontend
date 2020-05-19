0771ff8f9e18d958fb21c5c8dcc22ccc61501059
acao2002 <63988697+acao2002@users.noreply.github.com>
2020-05-19 11:04:55 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const myfirstelement = <h1>Hello </h1>
+
+const viggtitle = (
+  <table>
+    <tr>
+      <th>
+        Vigg 
+      </th>
+    </tr>
+      <th>
+        find your dreamed sneakers 
+      </th>      
+    <tr>
+
+    </tr>
+
+  </table>
+);
+
+ReactDOM.render(myfirstelement, document.getElementById('root'));
+ReactDOM.render(viggtitle, document.getElementById('header'));
