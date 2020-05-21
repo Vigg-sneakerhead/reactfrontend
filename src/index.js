504148b83395d5fbf4450e0854e57d76abac0e53
acao2002 <63988697+acao2002@users.noreply.github.com>
2020-05-21 16:33:26 -0400

    Add files via upload
    added icon and fonts...

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 const myfirstelement = <h1>Hello </h1>
 
@@ -20,5 +23,18 @@ const viggtitle = (
   </table>
 );
 
+const menu = (
+  <div className="container">
+    <div className="row">
+      <div className="col-6"> menu1 </div>
+      <div className="col-6"> 
+        <span>
+          <i className = 'fas fa-home'/>
+        </span> 
+      </div>
+    </div>
+  </div>
+)
+ReactDOM.render(menu, document.getElementById('menu'));
 ReactDOM.render(myfirstelement, document.getElementById('root'));
 ReactDOM.render(viggtitle, document.getElementById('header'));
