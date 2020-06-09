05855c2ef099e44ed5791c7ce67a57f25f74e17c
acao2002 <63988697+acao2002@users.noreply.github.com>
2020-06-08 21:32:02 -0400

    footer customized

--- src/components/HomePage/Footer/Footer.js
+++ src/components/HomePage/Footer/Footer.js
@@ -11,28 +11,31 @@ export default class Footer extends Component {
         return (
         <FooterContainer>
             <div className ="container-fluid footer">
-                <div className ="row">
-                    <div className = "col-lg-6 col-md-6 col-sm-12 mx-auto text-center my-1">
-                        <h3> Liên hệ chúng tôi </h3> 
-                        <h5>Email:<span><a href ='gmail.com'> viggmarket-place@gmail.com</a></span></h5>
-                        <h5>Sdt: (+84) 0396 549 315</h5>
-                        <h5>Trụ sở: 7 Trung Lang, Phường 12, Quận Tân Bình, Thành Phố Hồ Chí Minh</h5>
-                    </div>  
-                    <div className = "col-lg-6 col-md-6 col-sm-12 mx-auto text-center justify-content-center my-auto">
-                        <div className ="container">
-                            <div className = "row">
-                                <div className = 'col logo'>
-                                    <img src = {fb} className = "logo"/>
-                                </div>
-                                <div className = 'col logo'>
-                                    <img src = {insta} className = "logo"/>
-                                </div>
-                                <div className = 'col logo'>
-                                    <img src = {lkn} className = "logo"/>
-                                </div>
-                            </div>  
-                        </div>
-                    </div>                         
+                <div className ='container'>
+                    <div className ="row">      
+                        <div className = "col-lg-6 col-md-6 col-sm-12 text-left my-3">
+                            <h3 className ="info"> Liên hệ chúng tôi </h3> 
+                            <h5><i className ='fas fa-envelope'/> Email:<span><a href ='https://mail.google.com/'> viggmarket-place@gmail.com</a></span></h5>
+                            <h5><i className ='fas fa-phone'/> Sdt: (+84) 0396 549 315</h5>
+                            <h5><i className ='fas fa-map-marker'/> Trụ sở: 7 Trung Lang, Phường 12, Quận Tân Bình, </h5>
+                            <h5 > Thành Phố Hồ Chí Minh</h5>
+                        </div> 
+                        <div className = "col-lg-4 col-md-4 col-sm-12 mx-auto text-center justify-content-center my-auto">
+                            <div className ="container">
+                                <div className = "row">
+                                    <div className = 'col logo'>
+                                        <img src = {fb} className = "logo"/>
+                                    </div>
+                                    <div className = 'col logo'>
+                                        <img src = {insta} className = "logo"/>
+                                    </div>
+                                    <div className = 'col logo'>
+                                        <img src = {lkn} className = "logo"/>
+                                    </div>
+                                </div>  
+                            </div>
+                        </div>  
+                    </div>                       
                 </div>
             </div>  
         </FooterContainer>
