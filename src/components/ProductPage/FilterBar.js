5acad058fe3b780c2e81eaa9685d39220e61fa8b
acao2002 <63988697+acao2002@users.noreply.github.com>
2020-06-13 04:27:16 -0400

    added slider

--- src/components/ProductPage/FilterBar.js
+++ src/components/ProductPage/FilterBar.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
-
+import InputRange from 'react-input-range';
 import './ProductPage.css';
+import 'react-input-range/lib/css/index.css';
 
-export default class FilterBar extends Component {
-    render() {
-        
+export default class FilterBar extends Component {  
+    constructor(props) {
+        super(props);
+    
+        this.state = { value5: {
+            min: 1000000,
+            max: 2000000,
+          },
+        };
+      }
+    render() { 
         return (
             <div className ="ml-4">
                 <h5>Filter by</h5>
                 <hr/>
                 
-                <div className = "brand-toggle" onClick = {() => {
+                <div className = "component-toggle" onClick = {() => {
                         var x = document.getElementById("brands");
                         var y = document.getElementById("brandtoggle");
                         if (x.style.display !== "block") {
@@ -27,9 +36,43 @@ export default class FilterBar extends Component {
                 </div>
 
                 <div id ="brands" className ="brandlist">
-                    brands
+                    <div className ="font-weight-bold "> All </div>
+                    <div className ="font-weight-light brand"> Nike </div>
+                    <div className ="font-weight-light brand"> Adidas </div>
+                    <div className ="font-weight-light brand"> Jordon </div>
+                    <div className ="font-weight-light brand"> Yeezy </div>
+                    <div className ="font-weight-light brand"> Puma </div>
+                    <div className ="font-weight-light brand"> Vans </div>
+                    <div className ="font-weight-light brand"> Converse </div>
+                </div>
+                
+                <div className = "price-toggle component-toggle" onClick = {() => {
+                        var x = document.getElementById("price-range");
+                        var y = document.getElementById("brandtoggle2");
+                        if (x.style.display !== "block") {
+                            x.style.display = "block";
+                            y.innerHTML = "-"
+                        }
+                        else {
+                            x.style.display = "none";
+                            y.innerHTML = "+"
+                        
+                        } 
+                    }}>
+                    Giá <span id ="brandtoggle2" className ="toggle"> + </span>
+                </div>
+
+                <div id ="price-range" className ="brandlist">
+                    <InputRange
+                        draggableTrack
+                        maxValue={3000000}
+                        minValue={0}
+                        onChange={value => this.setState({ value5: value })}
+                        onChangeComplete={value => console.log(value)}
+                        value={this.state.value5} />
                 </div>
             </div>
+            
         )
     }
 }
